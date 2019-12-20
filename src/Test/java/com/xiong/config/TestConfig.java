c4ede927082139d456c6c1a5388e1deeb4927d62
tiannoyi <1347492956.com>
2019-12-20 15:53:59 +0800

    添加Junit测试例子

---
+++ src/Test/java/com/xiong/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.xiong.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: library
+ * @description:
+ * @author: XiongJun
+ * @create: 2019-12-20 15:51
+ **/
+@Configuration
+@ComponentScan("com.qf")
+public class TestConfig {
+}
