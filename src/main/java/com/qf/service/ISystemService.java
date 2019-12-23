408b84cb42be79eb4cb5bc3c9facf7006e2e71e4
AbyssWatchers666 <zxcv9234@outlook.com>
2019-12-23 21:54:19 +0800

    新增system上传图片，基本完成costs表的增删改查，mapper新增查询罚款金额方法

--- src/main/java/com/qf/service/ISystemService.java
+++ src/main/java/com/qf/service/ISystemService.java
@@ -1,12 +1,23 @@
 package com.qf.service;
 
+import com.qf.entity.Costs;
 import com.qf.entity.System;
+import com.qf.util.Page;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface ISystemService {
     Integer insertSystem(System system);
 
     //修改系统文件
-    Integer updateSystem(System system);
+    Integer updateSystem(String picName,System system);
+
+    //查询系统
+    List<System> selectSystemList();
+
+    //修改系统配置不更改图片
+    Integer updateSystemNoImg(System system);
+//    Page<System> selectSystemList(Integer currentPage, Integer pageSize);
 
 }
